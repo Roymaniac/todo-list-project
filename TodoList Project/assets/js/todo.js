189bbce842218e07a8101dba34cbd74c38796238
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2021-07-13 14:23:59 +0100

    Add files via upload
    fully responsive todo list web app

---
+++ TodoList Project/assets/js/todo.js
@@ -0,0 +1,27 @@
+//Check Todo's by clicking
+$("ul").on("click", "li", function(){
+   $(this).toggleClass("completed");
+});
+
+//Delete click
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//EventListner to get input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        console.log("Enter");
+        let todoText = $(this).val()
+        $(this).val("")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + " </li>");
+    }
+});
+
+// Toggle Input with Plus sign font
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle() 
+}); 
